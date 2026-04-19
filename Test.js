b0878f1adae6a00af2dd1ec5b8c1fd80b27f1ac6
Liouinz <linusdittmar@gmail.com>
2026-04-19 13:07:08 +0200

    Delete Test.js

--- Test.js
+++
@@ -1,8 +0,0 @@
-// => Hello world!
-console.log('Hello world!');
-
-// => Hello QuickRef.ME
-console.warn('hello %s', 'QuickRef.ME');
-
-// Prints error message to stderr
-console.error(new Error('Oops!'));
