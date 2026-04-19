705439e7aff3c913f2ba2987a1d8070204ee3e94
Liouinz <linusdittmar@gmail.com>
2026-04-19 13:04:42 +0200

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,8 @@
+// => Hello world!
+console.log('Hello world!');
+
+// => Hello QuickRef.ME
+console.warn('hello %s', 'QuickRef.ME');
+
+// Prints error message to stderr
+console.error(new Error('Oops!'));
